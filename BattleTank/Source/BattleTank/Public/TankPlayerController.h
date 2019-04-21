6259a0e0195cd320359a19fed22af19402f47078
faulks30 <mikejfaulkner@gmail.com>
2019-04-21 19:15:32 +1000

    C++ moved folders  - FUCK!!!!

--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -17,7 +17,7 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	
 
 public:
+	virtual void BeginPlay() override; 
+	
 	ATank *GetControlledTank() const;
-
-	virtual void BeginPlay() override;
 };
